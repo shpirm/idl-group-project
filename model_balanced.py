22d655046489575a3fa9a5cf3a3fd8429276b0f5
meraya <maria.shpir2509@gmail.com>
2023-03-23 11:03:03 +0200

    Update model_balanced.py

--- model_balanced.py
+++ model_balanced.py
@@ -2,10 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import random
-import time
-import json
 
-from collections import defaultdict
 import torch
 import torch.optim as optim
 import torchvision
@@ -19,18 +16,21 @@ from torch.autograd import Variable
 from torch.utils.data.sampler import SubsetRandomSampler
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.metrics import classification_report, precision_recall_fscore_support
+
+from transformers import ViTForImageClassification
 
 from PIL import Image
 import matplotlib.pyplot as plt
 
-NUM_CHANNELS = 3
-WIDTH = HEIGHT = 128
-NUM_CLASSES = len(os.listdir('./annotations'))
-N_EPOCHS = 50
-BATCH_SIZE_TRAIN = 50
+DATA_DIR = '../%s'
+
+IMG_SIZE = 224
+N_EPOCHS = 10
+LR = 0.0001
+BATCH_SIZE_TRAIN = 75
 BATCH_SIZE_TEST = 50
-LR = 0.01
+VAL_SIZE = 0.2
 
 # Multilabel sampler based on: https://github.com/issamemari/pytorch-multilabel-balanced-sampler
 class MultilabelBalancedRandomSampler(Sampler):
@@ -119,29 +119,34 @@ class MultilabelBalancedRandomSampler(Sampler):
         return len(self.indices)
 
 def get_labels(labels_dir, images_dir):
-    label_names = [os.path.splitext(f)[0] for f in os.listdir(labels_dir)]
-    image_indexes = [os.path.splitext(f)[0] for f in os.listdir(images_dir)]
+      """
+      Returns the names of the labels and a dictionary
+      where each image index corresponds to a list of its labels. 
+      """
 
-    label_indexes = {file_name: set() for file_name in label_names}
-    for file_name in label_names:
-        with open(os.path.join(labels_dir, file_name + '.txt')) as f:
-            for line in f: label_indexes[file_name].add(line.strip())
+      label_names = [os.path.splitext(f)[0] for f in os.listdir(labels_dir)]
+      image_indexes = [os.path.splitext(f)[0] for f in os.listdir(images_dir)]
 
-    image_labels = {index: [] for index in image_indexes}
-    for label in label_names:
-        for key in image_labels.keys():
-            image_labels[key].append(int(str(key[2:]) in label_indexes[label]))
+      label_indexes = {file_name: set() for file_name in label_names}
+      for file_name in label_names:
+          with open(os.path.join(labels_dir, file_name + '.txt')) as f:
+              for line in f: label_indexes[file_name].add(line.strip())
 
-    return label_names, image_labels
+      image_labels = {index: [] for index in image_indexes}
+      for label in label_names:
+          for key in image_labels.keys():
+              image_labels[key].append(int(str(key[2:]) in label_indexes[label]))
+
+      return label_names, image_labels
+
 
 class ImageDataset(Dataset):
-    
-    def __init__(self, image_dir: str, label_dir: str, train_transform = None, test_transform = None, train_idx = None, val_idx = None) -> None:
+    def __init__(self, image_dir: str, label_dir: str, train_transform = None, test_transform = None, train_idx = None) -> None:
         self.paths = [os.path.join(image_dir, file_name) for file_name in os.listdir(image_dir)]
+
         self.train_transform = train_transform
         self.test_transform = test_transform
-        self.train_idx = train_idx
-        self.val_idx = val_idx
+
         self.labels, self.idx_to_label = get_labels(label_dir, image_dir)
 
         class_probabilities = np.random.random([14])
@@ -150,7 +155,6 @@ class ImageDataset(Dataset):
         self.y = (
             np.random.random([len(self.paths), 14]) < class_probabilities
         ).astype(int)
-
 
     def load_image(self, index: int) -> Image.Image:
         "Opens an image via a path and returns it."
@@ -166,225 +170,189 @@ class ImageDataset(Dataset):
         image = self.load_image(index)
         image_name = os.path.splitext(os.path.basename(self.paths[index]))[0]
         label = torch.Tensor(self.idx_to_label[image_name])
-        l = len(self.paths)
         
-        if index in self.train_idx:
-            return self.train_transform(image), label
-        elif index in self.val_idx:
-            return self.test_transform(image), label
+        return (self.train_transform(image), label) if index in self.train_idx else (self.test_transform(image), label)
+
+def get_class_weights(dataset: ImageDataset):
+    "Returns the class weights of the given multi-label dataset."
+    weights = np.array([0 for labels in dataset.labels])
+    for image_key in dataset.idx_to_label:
+        weights += np.array(dataset.idx_to_label[image_key])
+
+    return torch.tensor([(len(dataset) - value)/value for value in weights])
 
 
 train_transform = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomResizedCrop(128),
-    transforms.RandomRotation(degrees=15),
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.RandomHorizontalFlip(p=0.2),
+    transforms.RandomVerticalFlip(p=0.2),
+    transforms.RandomResizedCrop(IMG_SIZE),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 ])
+
 test_transform = transforms.Compose([
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 ])
 
-def get_loader(batch_size, val_size):
+def get_loader(data: ImageDataset, batch_size, val_size):
 
-    indices = list(range(20000))
+    data_len = len(data)
+    indices = list(range(data_len))
+
     np.random.shuffle(indices)
-    split_idx = int(np.floor(val_size * 20000))
+    split_idx = int(val_size * data_len)
 
     train_idx, val_idx = indices[split_idx:], indices[:split_idx]
-    dataset = ImageDataset('./images', './annotations', train_transform, test_transform, train_idx, val_idx)
-    train_sampler = MultilabelBalancedRandomSampler(dataset.y, train_idx, class_choice="random")
-    valid_sampler = SubsetRandomSampler(val_idx)
 
-    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=train_sampler,)
-    valid_loader = DataLoader(dataset, batch_size=batch_size, sampler=valid_sampler,)
+    train_sampler = MultilabelBalancedRandomSampler(dataset.y, train_idx, class_choice="random")
+    data.train_idx = train_idx
+
+    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_idx))
+    valid_loader = DataLoader(dataset, batch_size=batch_size, sampler=SubsetRandomSampler(val_idx))
 
     return train_loader, valid_loader
 
-train_loader, val_loader = get_loader(BATCH_SIZE_TRAIN, 0.2)
+dataset = ImageDataset(DATA_DIR % 'images', DATA_DIR % 'annotations', train_transform, test_transform)
+train_loader, valid_loader = get_loader(dataset, BATCH_SIZE_TRAIN, VAL_SIZE)
 
-#model = models.resnet50(weights='DEFAULT')
-#model = models.resnet34(weights='DEFAULT')
-#model = models.resnet101(weights='DEFAULT')
-model = models.resnet152(weights='DEFAULT')
-#model = models.densenet161(weights='DEFAULT')
+model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+model.classifier = nn.Linear(in_features=768, out_features=len(dataset.labels), bias=True)
 
-for param in model.parameters():
-    param.requires_grad = False
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
 
-fc_layers = nn.Sequential(
-        nn.Linear(2048, 1024),
-        nn.ReLU(),
-        nn.BatchNorm1d(1024),
-        nn.Dropout(0.5),
-        nn.Linear(1024, 512),
-        nn.ReLU(),
-        nn.BatchNorm1d(512),
-        nn.Dropout(0.5),
-        nn.Linear(512, 256),
-        nn.ReLU(),
-        nn.BatchNorm1d(256),
-        nn.Dropout(0.5),
-        nn.Linear(256, 14)
-        )
+model = model.to(device)
+weights = get_class_weights(dataset)
 
-fc_classifier = nn.Sequential(
-        nn.Linear(2208, 1104),
-        nn.ReLU(),
-        nn.BatchNorm1d(1104),
-        nn.Dropout(0.5),
-        nn.Linear(1104, 552),
-        nn.ReLU(),
-        nn.BatchNorm1d(552),
-        nn.Dropout(0.5),
-        nn.Linear(552, 276),
-        nn.ReLU(),
-        nn.BatchNorm1d(276),
-        nn.Dropout(0.5),
-        nn.Linear(276, 14)
-        )
-
-#model.classifier = fc_classifier
-model.fc = fc_layers
-
-#model.fc = nn.Linear(2048, 14)
-model = model.cuda()
-
-loss = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
-#optimizer = optim.Adagrad(model.parameters(), lr=0.01)
-#lr_sch = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=20, eta_min=0.01)
+loss = nn.BCEWithLogitsLoss(reduction='none')
+optimizer = optim.Adam(model.parameters(), lr=LR)
 lr_sch = optim.lr_scheduler.LinearLR(optimizer, total_iters=10)
 
-
-def train(
-        n_epochs,
-        model,
-        train_loader,
-        valid_loader,
-        criterion,
-        optimizer
-        ):
-
+def train(n_epochs, model, train_loader, valid_loader, criterion, optimizer):
     for epoch in range(n_epochs):
         train_correct = 0
         valid_correct = 0
         train_loss, train_f1, train_rec, train_prec = 0.0, 0.0, 0.0, 0.0
         valid_loss, valid_f1, valid_rec, valid_prec = 0.0, 0.0, 0.0, 0.0
+
         for data, target in train_loader:
-            data, target = data.cuda(), target.cuda()
+            data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
-            output = model.forward(data)
+            output = model(data).logits
+
             loss = criterion(output, target)
+            # loss = (loss * weights.to(device))
+            loss = loss.mean()
             loss.backward()
             optimizer.step()
             lr_sch.step()
-            train_loss += loss.item()*data.size(0)
+
             pred = torch.sigmoid(output.data) > 0.48
             train_correct += pred.eq(target.data.view_as(pred)).sum()
-            train_f1 += f1_score(target.cpu().numpy(),
-                                 pred.cpu().numpy(),
-                                 average='samples',
-                                 zero_division=1
-                                 )*data.size(0)
-            train_rec += recall_score(target.cpu().numpy(),
-                                        pred.cpu().numpy(),
-                                        average='samples',
-                                        zero_division=1
-                                        )*data.size(0)
-            train_prec += precision_score(target.cpu().numpy(),
-                                        pred.cpu().numpy(),
-                                        average='samples',
-                                        zero_division=1
-                                        )*data.size(0)
+            prec, recall, f1, support = precision_recall_fscore_support(
+                target.cpu().numpy(), pred.cpu().numpy(), average='samples', zero_division=1)
+            
+            train_loss += loss * data.size(0)
+            train_f1   += f1 * data.size(0)
+            train_rec  += recall * data.size(0)
+            train_prec += prec * data.size(0)
         
-        train_loss = train_loss/(0.8*len(train_loader.sampler))
-        train_f1 = train_f1/(0.8*len(train_loader.sampler))
-        train_rec = train_rec/(0.8*len(train_loader.sampler))
-        train_prec = train_prec/(0.8*len(train_loader.sampler))
-        
-        print(f'Epoch: {epoch+1}/{n_epochs}',
+        train_loss /= len(train_loader.sampler)
+        train_f1 /= len(train_loader.sampler)
+        train_rec /= len(train_loader.sampler)
+        train_prec /= len(train_loader.sampler)
+
+        train_correct = train_correct.float() / (len(dataset.labels) * len(train_loader.sampler))
+        print(f'Epoch: {epoch+1} / {n_epochs}',
         f'Training Loss: {train_loss:.6f}',
-        f'Train acc: {train_correct.float()/(14*len(train_loader.sampler)):.6f}',
+        f'Train acc: {train_correct:.6f}',
         f'Train f1: {train_f1:.6f}',
         f'Train rec: {train_rec:.6f}',
         f'Train prec: {train_prec:.6f}')
+
+        # pred = np.array(pred.cpu(), dtype=float)
+        # print(classification_report(
+        #       target.cpu(),
+        #       pred,
+        #       output_dict=False,
+        #       target_names=dataset.labels
+        #       ))
         
         model.eval()
         with torch.no_grad():
             for data, target in valid_loader:
-                data, target = data.cuda(), target.cuda()
-                output = model.forward(data)
-                batch_loss = criterion(output, target)
-                valid_loss += batch_loss.item()*data.size(0)
+                data, target = data.to(device), target.to(device)
+                output = model(data).logits
+                loss = criterion(output, target).mean()
+
                 pred = torch.sigmoid(output.data) > 0.48
                 valid_correct += pred.eq(target.data.view_as(pred)).sum()
-                valid_f1 += f1_score(target.cpu().numpy(),
-                                     pred.cpu().numpy(),
-                                     average='samples',
-                                     zero_division=1
-                                     )*data.size(0)
-                valid_rec += recall_score(target.cpu().numpy(),
-                                            pred.cpu().numpy(),
-                                            average='samples',
-                                            zero_division=1
-                                            )*data.size(0)
-                valid_prec += precision_score(target.cpu().numpy(),
-                                            pred.cpu().numpy(),
-                                            average='samples',
-                                            zero_division=1
-                                            )*data.size(0)
+                prec, recall, f1, support = precision_recall_fscore_support(
+                    target.cpu().numpy(), pred.cpu().numpy(), average='samples', zero_division=1)
                 
+                valid_loss += loss * data.size(0)
+                valid_f1   += f1 * data.size(0)
+                valid_rec  += recall * data.size(0)
+                valid_prec += prec * data.size(0)
+            
+            valid_loss /= len(valid_loader.sampler)
+            valid_f1 /= len(valid_loader.sampler)
+            valid_rec /= len(valid_loader.sampler)
+            valid_prec /= len(valid_loader.sampler)
+
+            valid_correct = valid_correct.float() / (len(dataset.labels) * len(valid_loader.sampler))
+            print(f'Epoch: {epoch+1} / {n_epochs}',
+            f'Validation Loss: {valid_loss:.6f}',
+            f'Validation acc: {valid_correct:.6f}',
+            f'Validation f1: {valid_f1:.6f}',
+            f'Validation rec: {valid_rec:.6f}',
+            f'Validation prec: {valid_prec:.6f}')
+
+            # pred = np.array(pred.cpu(), dtype=float)
+            # print(classification_report(
+            #       target.cpu(),
+            #       pred,
+            #       output_dict=False,
+            #       target_names=dataset.labels
+            #       ))
                 
-        valid_loss = valid_loss/len(valid_loader.sampler)
-        valid_f1 = valid_f1/len(valid_loader.sampler)
-        valid_rec = valid_rec/len(valid_loader.sampler)
-        valid_prec = valid_prec/len(valid_loader.sampler)
-        
-        print(f'Epoch: {epoch+1}/{n_epochs}',
-        f'Validation Loss: {valid_loss:.6f}',
-        f'Validation acc: {valid_correct.float()/(14*len(valid_loader.sampler)):.6f}',
-        f'Validation f1: {valid_f1:.6f}',
-        f'Validation rec: {valid_rec:.6f}',
-        f'Validation prec: {valid_prec:.6f}')
         model.train()
 
     return train_loss, valid_loss
 
-train_loss, valid_loss = train(
-    10,
-    model,
-    train_loader,
-    val_loader,
-    loss,
-    optimizer
-    )
+train_loss, valid_loss = train(N_EPOCHS, model, train_loader, valid_loader, loss, optimizer)
 
-def test(model, test_loader):
-    test_loss = 0.0
-    test_f1 = 0.0
-    correct = 0
-    model.eval()
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.cuda(), target.cuda()
-            output = model.forward(data)
-            batch_loss = loss(output, target)
-            test_loss += batch_loss.item()*data.size(0)
-            pred = torch.sigmoid(output.data) > 0.48
-            correct += pred.eq(target.data.view_as(pred)).sum()
-            test_f1 += f1_score(target.cpu().numpy(),
-                                pred.cpu().numpy(),
-                                average='samples',
-                                zero_division=1
-                                )*data.size(0)
-    
-    test_loss = test_loss/len(test_loader.sampler)
-    test_f1 = test_f1/len(test_loader.sampler)
-    
-    print(f'Test Loss: {test_loss:.6f}... ',
-    f'Test Accuracy: {correct.float()/(14*len(test_loader.sampler)):.6f}',
-    f'Test f1: {test_f1:.6f}')
+# should be tested
 
-#test(model, test_loader)
+# def test(model, test_loader):
+#     test_loss = 0.0
+#     test_f1 = 0.0
+#     correct = 0
+#     model.eval()
+#     with torch.no_grad():
+#         for data, target in test_loader:
+#             data, target = data.cuda(), target.cuda()
+#             output = model.forward(data)
+#             batch_loss = loss(output, target)
+#             test_loss += batch_loss.item()*data.size(0)
+#             pred = torch.sigmoid(output.data) > 0.48
+#             correct += pred.eq(target.data.view_as(pred)).sum()
+#             test_f1 += f1_score(target.cpu().numpy(),
+#                                 pred.cpu().numpy(),
+#                                 average='samples',
+#                                 zero_division=1
+#                                 )*data.size(0)
+    
+#     test_loss = test_loss/len(test_loader.sampler)
+#     test_f1 = test_f1/len(test_loader.sampler)
+    
+#     print(f'Test Loss: {test_loss:.6f}... ',
+#     f'Test Accuracy: {correct.float()/(14*len(test_loader.sampler)):.6f}',
+#     f'Test f1: {test_f1:.6f}')
+
+# #test(model, test_loader) 
